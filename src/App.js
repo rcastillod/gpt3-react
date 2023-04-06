c2df41d073a7307b30cd012ba5ca149815814743
rcastillod <rodrigo.castillo@global3.cl>
2023-04-06 10:48:16 -0400

    feat: Files & folder structure done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
-export const App = () => {
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+import { CTA, Brand, Navbar } from './components'
+
+const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <div className='greadient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </div>
   )
 }
+
+export default App
