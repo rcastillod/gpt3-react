e8eb08cecee09458c5dcc086aa551f21dc82c64c
rcastillod <rodrigo.castillo@global3.cl>
2023-04-13 17:45:16 -0400

    feat: :construction: Working in features section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -5,7 +5,6 @@ const Feature = ({layout, title, text}) => {
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
-        <div />
         <h4>{title}</h4>
       </div>
       <div className="gpt3__features-container__feature-text">
