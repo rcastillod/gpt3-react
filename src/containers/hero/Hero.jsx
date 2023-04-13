e84b16fbd9b91c4928a42ee8e16a9cd3c90c3dfb
rcastillod <rodrigo.castillo@global3.cl>
2023-04-13 11:59:45 -0400

    feat: :sparkles: Hero section done
    Some changes in navbar and header component styles

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import './hero.css'
 
+// Assets
+import aiImage from '../../assets/ai.png';
+import people from '../../assets/people.png';
+
 const Hero = () => {
   return (
-    <div class="hero">
-      <div className="wrapper">
+    <div class="hero gradient__bg">
+      <div className="hero__wrapper">
         <div className="hero__content">
-          
+          <div className="hero__content-title">
+            <h1 className="gradient__text gradient__text--animation">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+          </div>
+          <div className="hero__content-description">
+            <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+          </div>
+          <div className="hero__content-form">
+            <form action="" method="post">
+              <input type="email" name="email" placeholder="Your Email Address" />
+              <button type="submit">Get Started</button>
+            </form>
+          </div>
+          <div className="hero__content-people">
+            <img src={people} alt="People" /> 
+            <p>1,600 people requested access a visit in last 24 hours</p>
+          </div>
+        </div>
+        <div className="hero__content-image">
+          <img src={aiImage} alt="AI Image" />
         </div>
       </div>
     </div>
