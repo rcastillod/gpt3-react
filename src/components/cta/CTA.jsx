fb4bff8fdf84be40597a74195a06354a9f19b55d
rcastillod <rodrigo.castillo@global3.cl>
2023-04-14 12:22:32 -0400

    feat: :sparkles: CTA component done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,19 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className="section__padding">
+      <div className="wrapper">
+        <div className="gpt3__cta">
+          <div className="gpt3__cta-content">
+            <p>Request Early Access to Get Started</p>
+            <h3>Register today & start exploring the endless possiblities.</h3>
+          </div>
+          <div className="gpt3__cta-button">
+            <a href="#">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
